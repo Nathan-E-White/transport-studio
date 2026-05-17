99919a1a15d21e333bb82e0dbb5cb35400f46c90
Nathan-E-White <nwnn4@mail.missouri.edu>
2026-05-17 04:54:51 -0500

    noinspection JSUnusedGlobalSymbol

--- packages/project-io/src/index.ts
+++ packages/project-io/src/index.ts
@@ -1,9 +1,11 @@
 import type { Project } from "@transport/domain";
 
+// noinspection JSUnusedGlobalSymbols
 export function serializeProject(project: Project): string {
   return JSON.stringify(project, null, 2);
 }
 
+// noinspection JSUnusedGlobalSymbols
 export function parseProject(json: string): Project {
   return JSON.parse(json) as Project;
 }
