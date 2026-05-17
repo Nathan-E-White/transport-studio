479771d5a2fe18d5654a0c6c7953155ce5545ef7
Nathan-E-White <nwnn4@mail.missouri.edu>
2026-05-17 04:57:25 -0500

    noinspection JSUnusedGlobalSymbol

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 }
